87b1a4813b362f7df6e366bd6b85c02e42a3be91
CAMILA ALVES <alvescamila87@gmail.com>
2025-02-17 21:20:18 -0300

    @autowired

--- calculator/src/main/java/com/senai/calculator/controllers/CalculatorController.java
+++ calculator/src/main/java/com/senai/calculator/controllers/CalculatorController.java
@@ -2,19 +2,28 @@ package com.senai.calculator.controllers;
 
 import com.senai.calculator.dtos.CalculatorDTO;
 import com.senai.calculator.dtos.ResultDTO;
+import com.senai.calculator.services.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
 
-    @PostMapping
-    public ResponseEntity<ResultDTO> sum(@RequestBody CalculatorDTO inputData) {
-        ResultDTO resultDTO = new ResultDTO();
-        resultDTO.setResult(inputData.getNumber1() + inputData.getNumber2());
+    @Autowired
+    CalculatorService calculatorService;
 
-        return ResponseEntity.ok().body(resultDTO);
+    @PostMapping("/sum")
+    public ResponseEntity<ResultDTO> sum(@RequestBody CalculatorDTO inputData) {
+//        ResultDTO resultDTO = new ResultDTO();
+//        resultDTO.setResult(inputData.getNumber1() + inputData.getNumber2());
+//
+//        return ResponseEntity.ok().body(resultDTO);
+        final var resultado =  calculatorService.executeSum(inputData);
+        return ResponseEntity.ok().body(resultado);
     }
 }
--- calculator/src/main/java/com/senai/calculator/services/CalculatorService.java
+++ calculator/src/main/java/com/senai/calculator/services/CalculatorService.java
@@ -1,4 +1,16 @@
 package com.senai.calculator.services;
 
+import com.senai.calculator.dtos.CalculatorDTO;
+import com.senai.calculator.dtos.ResultDTO;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
+
+    public ResultDTO executeSum(CalculatorDTO inputData) {
+        ResultDTO resultDTO = new ResultDTO();
+        resultDTO.setResult(inputData.getNumber1() + inputData.getNumber2());
+
+        return resultDTO;
+    }
 }
