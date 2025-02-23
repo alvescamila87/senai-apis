8cd7b3caa5547fca8fe00bab03035a3fdcc2ec15
Camila Alves <alvescamila87@gmail.com>
2025-02-23 12:16:44 -0300

    remove isolated methods and use else if with ignore case

--- convertionAPI/src/main/java/com/senai/convertionapi/services/ConversorService.java
+++ convertionAPI/src/main/java/com/senai/convertionapi/services/ConversorService.java
@@ -10,50 +10,20 @@ public class ConversorService {
     public RespostaDTO converterPorTipo(RequisicaoDTO requisicao) {
         var respostaDTO = transformarRequisicaoEmResposta(requisicao);
 
-        if(requisicao.getTipoConversao().equalsIgnoreCase("cm-m") || requisicao.getTipoConversao().equalsIgnoreCase("m-cm")) {
-            respostaDTO = converterComprimento(requisicao);
-        } else if(requisicao.getTipoConversao().equalsIgnoreCase("g-kg") || requisicao.getTipoConversao().equalsIgnoreCase("kg-g")) {
-            respostaDTO = converterMassa(requisicao);
-        } else if(requisicao.getTipoConversao().equalsIgnoreCase("c-f") || requisicao.getTipoConversao().equalsIgnoreCase("f-c")) {
-            respostaDTO = converterTemperatura(requisicao);
-        } else {
-            exibirMensagemDeErro(respostaDTO);
-        }
-
-        return respostaDTO;
-    }
-
-    private RespostaDTO converterComprimento(RequisicaoDTO requisicao) {
-        final var respostaDTO = transformarRequisicaoEmResposta(requisicao);
-
         if(requisicao.getTipoConversao().equalsIgnoreCase("cm-m")){
             respostaDTO.setValorConvertido(respostaDTO.getValorOriginal() / 100);
         } else if(requisicao.getTipoConversao().equalsIgnoreCase("m-cm")) {
             respostaDTO.setValorConvertido(respostaDTO.getValorOriginal() * 100);
-        }
-
-        return respostaDTO;
-    }
-
-    private RespostaDTO converterMassa(RequisicaoDTO requisicao) {
-        final var respostaDTO = transformarRequisicaoEmResposta(requisicao);
-
-        if(requisicao.getTipoConversao().equalsIgnoreCase("g-kg")) {
+        } else if(requisicao.getTipoConversao().equalsIgnoreCase("g-kg")) {
             respostaDTO.setValorConvertido(requisicao.getValor() / 1000);
         } else if (requisicao.getTipoConversao().equalsIgnoreCase("kg-g")) {
             respostaDTO.setValorConvertido(requisicao.getValor() * 1000);
-        }
-
-        return respostaDTO;
-    }
-
-    private RespostaDTO converterTemperatura(RequisicaoDTO requisicao) {
-        final var respostaDTO = transformarRequisicaoEmResposta(requisicao);
-
-        if(requisicao.getTipoConversao().equalsIgnoreCase("c-f")) {
+        } else if(requisicao.getTipoConversao().equalsIgnoreCase("c-f")) {
             respostaDTO.setValorConvertido((requisicao.getValor() * 9/5) + 32);
         } else if(requisicao.getTipoConversao().equalsIgnoreCase("f-c")) {
             respostaDTO.setValorConvertido((requisicao.getValor() - 32) * 5/9);
+        } else {
+            exibirMensagemDeErro(respostaDTO);
         }
 
         return respostaDTO;
