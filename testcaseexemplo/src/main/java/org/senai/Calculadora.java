49ddb9a21b12d093096bf830a3bfaa640d05dafb
CAMILA ALVES <alvescamila87@gmail.com>
2025-02-25 21:28:46 -0300

    coverage test calc

--- testcaseexemplo/src/main/java/org/senai/Calculadora.java
+++ testcaseexemplo/src/main/java/org/senai/Calculadora.java
@@ -5,4 +5,19 @@ public class Calculadora {
     public int soma(int num1, int num2) {
         return num1 + num2;
     }
+
+    public double divide(int num1, int num2) {
+        if(num2 == 0) {
+            throw new IllegalArgumentException("Não é possível divisão por zero.");
+        }
+        return (double) num1 / num2;
+    }
+
+    public int subtracao(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiplica(int num1, int num2) {
+        return num1 * num2;
+    }
 }
--- testcaseexemplo/src/test/java/TesteCalculadora.java
+++ testcaseexemplo/src/test/java/TesteCalculadora.java
@@ -2,13 +2,51 @@ import org.junit.Test;
 import org.senai.Calculadora;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class TesteCalculadora {
 
     @Test
-    public void testeSoma() {
+    public void testeSomaPositivo() {
         Calculadora calc = new Calculadora();
         int resultado = calc.soma(5,2);
         assertEquals(7, resultado);
+    }
+
+    @Test
+    public void testeSomaNegativo() {
+        Calculadora calc = new Calculadora();
+        int resultado = calc.soma(-5, 2);
+        assertEquals(-3, resultado);
+    }
+
+
+    @Test
+    public void testeDivisaoOk() {
+        Calculadora calc  = new Calculadora();
+        double resultado = calc.divide(10, 3);
+        assertEquals(3.3, resultado, 0.1);
+    }
+
+    @Test
+    public void testeDivisaoNOk() {
+        Calculadora calc = new Calculadora();
+        assertThrows(IllegalArgumentException.class, () -> {
+            calc.divide(5, 0);
+        });
+    }
+
+    @Test
+    public void testeSubstracao() {
+        Calculadora calc = new Calculadora();
+        int resultado = calc.subtracao(10, 3);
+        assertEquals(7, resultado);
+    }
+
+    @Test
+    public void testeMultiplicacao() {
+        Calculadora calc = new Calculadora();
+        int resltado = calc.multiplica(5, 2);
+        assertEquals(10, resltado);
     }
 }
