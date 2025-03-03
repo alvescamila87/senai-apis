ba7fca8ec9f7fffbc70d9507a5afbd931a9483ae
CAMILA ALVES <alvescamila87@gmail.com>
2025-03-03 20:49:45 -0300

    create: model and repository

---
+++ user-api-database/src/main/java/com/senai/user_database/models/Usuario.java
@@ -0,0 +1,29 @@
+package com.senai.user_database.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+}
---
+++ user-api-database/src/main/java/com/senai/user_database/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.senai.user_database.repositories;
+
+import com.senai.user_database.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
