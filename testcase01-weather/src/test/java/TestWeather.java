9a8f101bf182a83bb7abf53f862978fb3e728593
Camila Alves <alvescamila87@gmail.com>
2025-03-03 14:41:02 -0300

    feat: create test to default scenario

--- testcase01-weather/src/test/java/TestWeather.java
+++ testcase01-weather/src/test/java/TestWeather.java
@@ -10,14 +10,47 @@ public class TestWeather {
     public void when_summer() {
         Weather weather = new Weather();
         int result = weather.checkWeather(1);
-        assertEquals(5, result);
+        assertEquals(1, result);
     }
 
     @Test
-    public void when_summer_nok() {
+    public void when_autumn() {
         Weather weather = new Weather();
-        assertThrows(IllegalArgumentException.class, () -> {
+        int result = weather.checkWeather(2);
+        assertEquals(2, result);
+    }
+
+
+    @Test
+    public void when_spring() {
+        Weather weather = new Weather();
+        int result = weather.checkWeather(3);
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void when_winter() {
+        Weather weather = new Weather();
+        int result = weather.checkWeather(4);
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void when_default_negative_number_nok() {
+        Weather weather = new Weather();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            weather.checkWeather(-1);
+        });
+        assertEquals("404 Not found!", exception.getMessage());
+    }
+
+    @Test
+    public void when_default_greater_than_four_nok() {
+        Weather weather = new Weather();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             weather.checkWeather(5);
         });
+
+        assertEquals("404 Not found!", exception.getMessage());
     }
 }
