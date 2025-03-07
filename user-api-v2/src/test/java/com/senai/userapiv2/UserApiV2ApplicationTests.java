06a87d9d223ae370378dfe420e5cf0f1517a24b4
Camila Alves <alvescamila87@gmail.com>
2025-03-07 20:09:29 -0300

    v2: database project with auth

---
+++ user-api-v2/src/test/java/com/senai/userapiv2/UserApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.userapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
