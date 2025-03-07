24e473f983b8dd93b8eff7a8b01563e4b6b7750e
Camila Alves <alvescamila87@gmail.com>
2025-03-07 20:23:23 -0300

    feat: model

---
+++ user-api-v2/src/main/java/com/senai/userapiv2/models/UsuarioModel.java
@@ -0,0 +1,29 @@
+package com.senai.userapiv2.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+}
