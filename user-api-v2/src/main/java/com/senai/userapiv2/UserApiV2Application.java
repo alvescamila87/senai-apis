28bcdc729b559f4149844d88e8f3fb218f474687
Camila <alvescamila87@gmail.com>
2025-03-08 20:11:19 -0300

    feat: adjust lombok and post

--- user-api-v2/src/main/java/com/senai/userapiv2/UserApiV2Application.java
+++ user-api-v2/src/main/java/com/senai/userapiv2/UserApiV2Application.java
@@ -1,5 +1,6 @@
 package com.senai.userapiv2;
 
+import com.senai.userapiv2.models.UsuarioModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,12 @@ public class UserApiV2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiV2Application.class, args);
+
+		UsuarioModel usuarioModel = new UsuarioModel(1L,"Camila", "alves", "123");
+		System.out.println(usuarioModel.getId());
+		System.out.println(usuarioModel.getNome());
+		System.out.println(usuarioModel.getLogin());
+		System.out.println(usuarioModel.getSenha());
 	}
 
 }
