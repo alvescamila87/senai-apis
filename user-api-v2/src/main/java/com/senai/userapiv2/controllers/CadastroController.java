62322446aa1ec8857a5ed73c3e0d30d6c7a2a1b3
CAMILA ALVES <alvescamila87@gmail.com>
2025-04-10 21:52:39 -0300

    feat: sighup

---
+++ user-api-v2/src/main/java/com/senai/userapiv2/controllers/CadastroController.java
@@ -0,0 +1,34 @@
+package com.senai.userapiv2.controllers;
+
+import com.senai.userapiv2.dtos.RequisicaoDTO;
+import com.senai.userapiv2.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class CadastroController {
+
+    @Autowired
+    UsuarioService service;
+
+    @GetMapping
+    public String obterCadastro(Model model) {
+
+        RequisicaoDTO signup = new RequisicaoDTO();
+        model.addAttribute("signup", signup);
+
+        return "signup";
+    }
+
+    @PostMapping
+    public String cadastrarUsuario(@ModelAttribute ("signup") RequisicaoDTO requisicaoDTO) {
+
+
+    }
+}
