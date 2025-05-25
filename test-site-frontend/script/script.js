5cf050157cd59bd8e0d52e7744e5ce3213679160
Camila <alvescamila87@gmail.com>
2025-05-25 17:40:16 -0300

    feat: hover

--- test-site-frontend/script/script.js
+++ test-site-frontend/script/script.js
@@ -12,13 +12,6 @@ document
     form.classList.add("was-validated");
   });
 
-/** LOGIN */ // Modo escuro
-function toggleDarkMode() {
-  const html = document.documentElement;
-  const current = html.getAttribute("data-bs-theme");
-  html.setAttribute("data-bs-theme", current === "dark" ? "light" : "dark");
-}
-
 /** LOGIN */ // validação de senha
 const form = document.getElementById("form_cadastro");
 
@@ -31,17 +24,3 @@ form.addEventListener("submit", function (event) {
     alert("As senhas são diferentes!");
   }
 });
-
-/** CADASTRO */
-
-function toggleTheme() {
-  const html = document.documentElement;
-  const currentTheme = html.getAttribute("data-bs-theme");
-  const newTheme = currentTheme === "dark" ? "light" : "dark";
-  html.setAttribute("data-bs-theme", newTheme);
-
-  // Alterna ícone
-  const icon = document.querySelector(".theme-toggle-btn i");
-  icon.classList.toggle("bi-moon-stars-fill");
-  icon.classList.toggle("bi-brightness-high-fill");
-}
